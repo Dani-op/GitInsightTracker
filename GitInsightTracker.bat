64a0ca7497fb853eb78a479accc7f2c737c14745
Durr E Muhammad Ghauri (1000103499) <GHAUD1@student.op.ac.nz>
2023-06-16 20:54:05 +1200

    feat: allowing user to select Y/N to the commands

--- GitInsightTracker.bat
+++ GitInsightTracker.bat
@@ -1,50 +1,93 @@
 @echo off
+cls
 echo.
 echo ======================================================
 echo       GIT INSIGHTS SCRIPT (Developed by Dani-op)
 echo ======================================================
 echo.
+
+:commitCount
 echo ======================================================
 echo                  Counting Git Commits:
 echo ======================================================
-git shortlog -s -n --all --no-merges
+echo Do you want to continue? (Y/N)
+set /p UserInput=
+if /i "%UserInput%"=="Y" (
+    git shortlog -s -n --all --no-merges
+    git shortlog -s -n --all --no-merges > temp.txt
+) else (
+    goto asciiGraph
+)
 echo Press any key to continue . . .
 pause >nul
+
+:asciiGraph
+echo.
+echo ======================================================
+echo 	=== ASCII Bar Graph for Commit Counts: ===
+echo ======================================================
+echo Do you want to continue? (Y/N)
+set /p UserInput=
+if /i "%UserInput%"=="Y" (
+    setlocal enabledelayedexpansion
+    for /f "tokens=1,2" %%a in (temp.txt) do (
+        set "bar="
+        for /l %%i in (1,1,%%a) do set "bar=!bar!#"
+        echo %%b : !bar!
+    )
+    del temp.txt
+) else (
+    goto commitMessages
+)
+echo Press any key to continue . . .
+pause >nul
+
+:commitMessages
+echo.
+echo ======================================================
+echo     === Commit Messages and Usernames from each Contributor: ===
+echo ======================================================
+echo Do you want to continue? (Y/N)
+set /p UserInput=
+if /i "%UserInput%"=="Y" (
+    git shortlog --pretty=short --no-merges
+) else (
+    goto detailedCommit
+)
+echo Press any key to continue . . .
+pause >nul
+
+:detailedCommit
 echo.
 echo ======================================================
 echo     === Obtaining Detailed Commit Messages: ===
 echo ======================================================
-git log --pretty=format:"%%cn committed %%h on %%cd" --no-merges --full-history --oneline --no-notes --branches --source
+echo Do you want to continue? (Y/N)
+set /p UserInput=
+if /i "%UserInput%"=="Y" (
+    git log --pretty=format:"%%cn committed %%h on %%cd" --no-merges --full-history --oneline --no-notes --branches --source
+) else (
+    goto visualizingCommits
+)
 echo Press any key to continue . . .
 pause >nul
+
+:visualizingCommits
 echo.
 echo ======================================================
 echo 	=== Visualizing Commits with Graph: ===
 echo ======================================================
-git log --pretty=format:"%%cn committed %%h on %%cd" --no-merges --full-history --oneline --no-notes --branches --source --graph
+echo Do you want to continue? (Y/N)
+set /p UserInput=
+if /i "%UserInput%"=="Y" (
+    git log --pretty=format:"%%cn committed %%h on %%cd" --no-merges --full-history --oneline --no-notes --branches --source --graph
+) else (
+    goto endScript
+)
 echo Press any key to continue . . .
 pause >nul
-echo.
-echo ======================================================
-echo 	=== Log Statistics: ===
-echo ======================================================
-git log --stat
-echo Press any key to continue . . .
-pause >nul
-echo.
-echo ======================================================
-echo 	=== Changes Since Last Commit: ===
-echo ======================================================
-git diff --stat
-echo Press any key to continue . . .
-pause >nul
-echo.
-echo ======================================================
-echo 	=== Show Branches and Their Commits: ===
-echo ======================================================
-git show-branch
-echo Press any key to continue . . .
-pause >nul
+
+:endScript
 echo.
 echo === End of Script ===
 pause >nul
